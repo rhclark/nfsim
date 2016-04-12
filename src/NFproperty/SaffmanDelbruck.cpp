543ef5fefc921ae2f036c987cbb473af77378ded
Jose Juan Tapia <jjtapia@gmail.com>
2016-04-11 23:01:37 -0400

    another missing file, this one is the last one

---
+++ src/NFproperty/SaffmanDelbruck.cpp
@@ -0,0 +1,47 @@
+#include "NFcore.hh"
+#include "NFproperty.hh"
+#include "NFutil.hh"
+
+using namespace std;
+using namespace NFcore;
+
+
+void SaffmanDelbruck::getValue(string& value){
+    value = "";
+}
+
+double SaffmanDelbruck::getDiffusionValue(){
+    //KB*T*LOG((mu_PM*h/(Rc*(mu_EC+mu_CP)/2))-gamma)/(4*PI*mu_PM*h)
+    double kb = NFutil::convertToDouble(this->getProperty("kb")->getValue());
+    double t =  NFutil::convertToDouble(this->getProperty("temperature")->getValue());
+    double rc = NFutil::convertToDouble(this->getProperty("rs")->getValue());
+    double gamma = NFutil::convertToDouble(this->getProperty("gamma")->getValue());
+    double width = NFutil::convertToDouble(this->getProperty("width")->getValue());
+    double viscosity = NFutil::convertToDouble(this->getProperty("viscosity")->getValue());
+    double surrounding_viscosity = getViscositySurroundingVolume();
+    double size = 1;
+    double pi = 3.141592;
+
+
+    return kb*t*log((viscosity*width/(rc*surrounding_viscosity))-gamma)/(4*pi*viscosity*width);
+    
+    
+    return 0;
+}
+
+double SaffmanDelbruck::getViscositySurroundingVolume(){
+    Compartment* compartment = dynamic_cast<Compartment*>(this->getContainer()->getContainer());
+    if(!compartment){
+        return -1.0;
+    }
+    //this might be worth doing in a different way once we have a more complex hierarchy
+    System* system = dynamic_cast<System*>(compartment->getContainer());
+    vector<Compartment*> children;
+    system->getAllCompartments().getCompartmentChildren(compartment->getName(), children);
+    Compartment* parent = system->getAllCompartments().getCompartment(compartment->getOutside());
+
+    double parentViscosity = NFutil::convertToDouble(parent->getProperty("viscosity")->getValue());
+    double childViscosity = NFutil::convertToDouble(children.at(0)->getProperty("viscosity")->getValue());
+
+    return (parentViscosity + childViscosity)/2;
+}
