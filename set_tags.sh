7f2d517158966b2b113502387b055b3a7453522b
Robert Clark <rhclark.pitt@gmail.com>
2017-05-12 12:28:53 -0400

    qq
    qq

--- set_tags.sh
+++ set_tags.sh
@@ -1,6 +1,10 @@
 BRANCH="master"
 
 echo -e "Entering  set_tags.sh.\n"
+echo -e "TRAVIS_PULL_REQUEST = " $TRAVIS_PULL_REQUEST
+echo -e "TRAVIS_BRANCH = " $TRAVIS_BRANCH
+echo -e "TRAVIS_TAG = " $TRAVIS_AG
+echo -e "BRANCH = " $BRANCH
 
 if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
   if [ "$TRAVIS_PULL_REQUEST" = false ]; then
