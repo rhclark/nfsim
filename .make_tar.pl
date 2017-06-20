72370b209f1d6e7e96b950f18edbec49d741e58c
Robert Clark <rhclark.pitt@gmail.com>
2017-06-20 12:22:57 -0400

    de
    de

--- .make_tar.pl
+++ .make_tar.pl
@@ -61,7 +61,7 @@ if ($platform eq "linux") {
     if ($platform eq "x86") {
       $zip_type = ".zip";  $travis_os = "Win32";
       system('copy  ./bin/NFsim.exe  ./bin/NFsim.x86.exe');
-      my $archive_file = "./build/NFsim-source-".$platform.$zip_type;
+      my $archive_file = ".\build\NFsim-source-".$platform.$zip_type;
       print "\nCreating NFsim".$platform.".exe source archive:\n";
       system("7z a  ${archive_file} doc models src test tools validate CMakeLists.txt LICENSE.txt README.txt makefile NFsim_manual_v1.12.pdf ");
       system("pwd");
