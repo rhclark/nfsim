d9b9709dad9e0b9cce1e9518d1423c3f2bd25e1a
Robert Clark <rhclark.pitt@gmail.com>
2017-07-07 17:44:01 -0400

    0
    0

--- src/NFinput/NFinput.cpp
+++ src/NFinput/NFinput.cpp
@@ -132,6 +132,7 @@ System * NFinput::initializeFromXML(
 			return NULL;
 		}
 
+	/* Should not be here */	return s; /* Should not be here */
 	cout<<" iXML 7 "<<endl<<endl;
 
 		if(!verbose) cout<<"-";
